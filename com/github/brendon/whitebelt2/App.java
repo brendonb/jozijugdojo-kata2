46c2e05adc1fd3c9a54f383c078a478e0fcd436c
brendonb <brendon.barends@gmail.com>
2014-07-01 10:05:41 +0200

    third commit

---
+++ com/github/brendon/whitebelt2/App.java
@@ -0,0 +1,24 @@
+package com.github.brendon.whitebelt2;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
+public class App{
+
+public static void main(String[] args){
+
+LocalDateTime now  =  LocalDateTime.now();
+
+System.out.println("It is now : "+now);
+
+LocalDateTime tenDaysFromNow = now.plus(10, ChronoUnit.DAYS);
+
+System.out.println("Now did not change , it is immutable (will never change) : "+now);
+
+System.out.println("Ten days from now is "+tenDaysFromNow);
+
+}
+}
+
+
+
